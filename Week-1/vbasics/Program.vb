a319535ad58d5d8b6c6c32805f830cd3cedaf6ae
Marielle Nolasco <marielletnolasco@gmail.com>
2023-02-08 12:56:27 -0500

    guessing game v2

--- Week-1/vbasics/Program.vb
+++ Week-1/vbasics/Program.vb
@@ -19,8 +19,11 @@ Module Program
     ' using dim, the syntax is dim name of var as datatype
     ' the greeter function returns a string that we are passing to the 
     ' greeting variable 
-    dim greeting as String =  greeter("casual")
-    Console.WriteLine(greeting)
+    ' dim greeting as String =  greeter("casual")
+    ' Console.WriteLine(greeting)
+    ' GuessingGame(1)
+    ' Counter(3)
+    GuessingGamev2()
     End Sub
     ' you set the datatype with the as keyword 
     Function greeter(greeting_type as String) As String
@@ -39,4 +42,63 @@ Module Program
         End Select
     End Function
     
+    Sub GuessingGame(guess as Integer)
+        ' We'll use the random class to generate a random number
+        dim rnd as Random = new Random()
+        dim mysteryNumber as integer = rnd.Next(1,2)
+        ' we use <> to represent non equality ie !=
+        ' if block: 
+        If guess <> mysteryNumber Then 
+            Console.WriteLine("Wrong guess :<")    
+        Else Console.WriteLine("Good guess :>")
+        End if
+    End Sub
+
+    Sub Counter(stopping_point as Integer)
+        dim start as Integer = 0
+        ' while loop
+        While  start < stopping_point
+            Console.WriteLine(start)
+            start = start + 1
+        End While 
+
+        'for loop
+        'the lower and upper bounds are included in the iteration
+        For index As integer = 0 To stopping_point
+            Console.WriteLine(index)
+        Next
+        
+    End Sub
+    Sub GuessingGamev2()
+        ' create a new instance of the random class
+        dim rnd as Random = new Random()
+        dim mysteryNumber as Integer = rnd.Next(1,100)
+        Console.WriteLine("Enter guess: ")
+        dim guess as String = Console.ReadLine()
+        ' integer struct has a method called Parse that converts strings to their
+        ' numeric values 
+        ' TODO: add exception handling 
+        dim guessNum as Integer = 0
+        Try
+            guessNum = Integer.Parse(guess)
+        Catch ex As Exception
+            Console.WriteLine("input is invalid. try again")
+        End Try
+        
+        
+        While guessNum <> mysteryNumber
+            ' we wanna keep guessing
+            If guessNum < mysteryNumber Then
+                Console.WriteLine("Too low")
+            Else Console.WriteLine("Too high")
+            End If
+            Console.WriteLine("Enter guess: ")
+            Try
+                guessNum = Integer.Parse(Console.ReadLine())
+            Catch ex As Exception
+                Console.WriteLine("invalid input, try again")
+            End Try
+        End While
+        Console.WriteLine("Congratulations! You guessed correctly")
+    End Sub
 End Module
